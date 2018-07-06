cc66bf5f871952192a3f9849b47aa62eca8ceda2
mfbradley <mbradleystylist@gmail.com>
2018-07-06 09:02:39 -0400

    wireframe with placeholder images index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+// Initialize Materialize Components
+$(document).ready(function () {
+  // $('.sidenav').sidenav();
+  
+  // $('.slider').slider();
+  // $('.carousel').carousel();
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+});
