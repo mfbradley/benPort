7a453d587cf22713c56fe1bd55543f4153225b42
mfbradley <mbradleystylist@gmail.com>
2018-07-11 13:53:35 -0400

    added nodemon package

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var exphbs = require('express-handlebars');
 var path = require("path");
 
 var app = express();
-
-var PORT = process.env.PORT || 8080;
-
+var PORT = process.env.PORT || 8081;
 app.use(express.static('public'))
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './index.html' ))
 })
+
+
 
 app.listen(PORT, function() {
     console.log('App listening on port:' + PORT);
