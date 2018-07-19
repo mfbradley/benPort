f5ecc140e468daaf8a8965838255e10dbe1d49b4
mfbradley <mbradleystylist@gmail.com>
2018-07-19 10:30:19 -0400

    click functions for sideNav li in portfolio section

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -4,13 +4,40 @@ $(document).ready(function () {
 	$('.about').hide();
 	$('.contact').hide();
 
+	$('.laydownsNav').on('click', function() {
+		$('.imagesDiv').show();
+		$('.contact').hide();
+		$('.about').hide();
+	})
+
+	$('.editorialNav').on('click', function() {
+		$('.imagesDiv').show();
+		$('.contact').hide();
+		$('.about').hide();
+	})
+
+	$('.tableTopNav').on('click', function() {
+		$('.imagesDiv').show();
+		$('.contact').hide();
+		$('.about').hide();
+	})
+
+	$('.gifsNav').on('click', function() {
+		$('.imagesDiv').show();
+		$('.contact').hide();
+		$('.about').hide();
+	})
+
 	$('.aboutNav').on('click', function() {
 		$('.imagesDiv').hide();
+		$('.contact').hide();
 		$('.about').show();
+
 	})
 	
 	$('.contactNav').on('click', function() {
 		$('.imagesDiv').hide();
+		$('.about').hide();
 		$('.contact').show();
 	})
 
