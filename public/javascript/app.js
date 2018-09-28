dcc70d08107a11d37ca059cb25e0e8f9e16e6ea9
mfbradley <mbradleystylist@gmail.com>
2018-09-28 11:12:42 -0400

    adding imagery and updated click functions

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,75 +1,101 @@
 
 $(document).ready(function () {
-	var images = [
-		{
-			src: "../images/loch_raven.jpg",
-			alt: "image1"
-		},
+	// LONGTERM PLAN - DYNAMICALLY GENERATE IMAGES FROM STORAGE
+	// ---------------------------------------------------------
+	// var images = [
+	// 	{
+	// 		src: "../images/loch_raven.jpg",
+	// 		alt: "image1"
+	// 	},
 
-		{
-			src: "../images/bmore.jpg",
-			alt: "image2"
-		}
-	]
-	
-	
-	function showImages() {
-		for (var i = 0; i < images.length; i++) {
-			var newCol = $("<div class='col m3'></div>")
-			var newImg = $("<img>");
-			newImg.attr("src", images[i].src)
-			newImg.attr("alt", images[i].alt)
-
-			newCol.append(newImg);
-			$(".imagesDiv").append(newCol)
-		}
+	// 	{
+	// 		src: "../images/bmore.jpg",
+	// 		alt: "image2"
+	// 	}
+	// ]
 
 
-	}
+	// function showImages() {
+	// 	for (var i = 0; i < images.length; i++) {
+	// 		var newCol = $("<div class="col m3"></div>")
+	// 		var newImg = $("<img>");
+	// 		newImg.attr("src", images[i].src)
+	// 		newImg.attr("alt", images[i].alt)
 
-	showImages();
+	// 		newCol.append(newImg);
+	// 		$(".imagesDiv").append(newCol)
+	// 	}
 
-	$('.imagesDiv').show();
-	$('.about').hide();
-	$('.contact').hide();
 
-	$('.laydownsNav').on('click', function () {
-		
-		$('.imagesDiv').show();
-		showImages();
-		$('.contact').hide();
-		$('.about').hide();
+	// }
+
+	// showImages();
+	// -----------------------------------------------------------
+
+	$(".imagesDiv").show();
+	$(".laydownsDiv").hide();
+	$(".tableTopDiv").hide();
+	$(".editorialDiv").hide();
+	$(".gifDiv").hide();
+	$(".about").hide();
+	$(".contact").hide();
+
+	$(".laydownsNav").on("click", function () {
+
+		$(".laydownsDiv").show();
+		$(".imagesDiv").hide();
+		$(".tableTopDiv").hide();
+		$(".editorialDiv").hide();
+		$(".gifDiv").hide();
+		$(".contact").hide();
+		$(".about").hide();
 	})
 
-	$('.editorialNav').on('click', function () {
-		$('.imagesDiv').show();
-		$('.contact').hide();
-		$('.about').hide();
+	$(".editorialNav").on("click", function () {
+		$(".imagesDiv").hide();
+		$(".laydownsDiv").hide();
+		$(".editorialDiv").show();
+		$(".tableTopDiv").hide();
+		$(".gifDiv").hide();
+		$(".contact").hide();
+		$(".about").hide();
 	})
 
-	$('.tableTopNav').on('click', function () {
-		$('.imagesDiv').show();
-		$('.contact').hide();
-		$('.about').hide();
+	$(".tableTopNav").on("click", function () {
+		$(".imagesDiv").hide();
+		$(".tableTopDiv").show();
+		$(".editorialDiv").hide();
+		$(".laydownsDiv").hide();
+		$(".gifDiv").hide();
+		$(".contact").hide();
+		$(".about").hide();
 	})
 
-	$('.gifsNav').on('click', function () {
-		$('.imagesDiv').show();
-		$('.contact').hide();
-		$('.about').hide();
+	$(".gifsNav").on("click", function () {
+		$(".gifDiv").show();
+		$(".imagesDiv").hide();
+		$(".tableTopDiv").hide();
+		$(".editorialDiv").hide();
+		$(".laydownsDiv").hide();
+		$(".contact").hide();
+		$(".about").hide();
 	})
 
-	$('.aboutNav').on('click', function () {
-		$('.imagesDiv').hide();
-		$('.contact').hide();
-		$('.about').show();
+	$(".aboutNav").on("click", function () {
+		$(".imagesDiv").hide();
+		$(".laydownsDiv").hide();
+		$(".editorialDiv").hide();
+		$(".tableTopDiv").hide();
+		$(".gifDiv").hide();
+		$(".contact").hide();
+		$(".about").show();
 
 	})
 
-	$('.contactNav').on('click', function () {
-		$('.imagesDiv').hide();
-		$('.about').hide();
-		$('.contact').show();
+	$(".contactNav").on("click", function () {
+		$(".imagesDiv").hide();
+		$(".about").hide();
+		$(".contact").show();
 	})
 
 
