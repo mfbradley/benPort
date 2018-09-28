42e18527940a5cedc94926b9a248d23585dd14ec
mfbradley <mbradleystylist@gmail.com>
2018-09-28 10:27:37 -0400

    updated folder structure/file tree

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,41 +1,72 @@
-// Initialize Materialize Components
+
 $(document).ready(function () {
+	var images = [
+		{
+			src: "../images/loch_raven.jpg",
+			alt: "image1"
+		},
+
+		{
+			src: "../images/bmore.jpg",
+			alt: "image2"
+		}
+	]
+	
+	
+	function showImages() {
+		for (var i = 0; i < images.length; i++) {
+			var newCol = $("<div class='col m3'></div>")
+			var newImg = $("<img>");
+			newImg.attr("src", images[i].src)
+			newImg.attr("alt", images[i].alt)
+
+			newCol.append(newImg);
+			$(".imagesDiv").append(newCol)
+		}
+
+
+	}
+
+	showImages();
+
 	$('.imagesDiv').show();
 	$('.about').hide();
 	$('.contact').hide();
 
-	$('.laydownsNav').on('click', function() {
+	$('.laydownsNav').on('click', function () {
+		
+		$('.imagesDiv').show();
+		showImages();
+		$('.contact').hide();
+		$('.about').hide();
+	})
+
+	$('.editorialNav').on('click', function () {
 		$('.imagesDiv').show();
 		$('.contact').hide();
 		$('.about').hide();
 	})
 
-	$('.editorialNav').on('click', function() {
+	$('.tableTopNav').on('click', function () {
 		$('.imagesDiv').show();
 		$('.contact').hide();
 		$('.about').hide();
 	})
 
-	$('.tableTopNav').on('click', function() {
+	$('.gifsNav').on('click', function () {
 		$('.imagesDiv').show();
 		$('.contact').hide();
 		$('.about').hide();
 	})
 
-	$('.gifsNav').on('click', function() {
-		$('.imagesDiv').show();
-		$('.contact').hide();
-		$('.about').hide();
-	})
-
-	$('.aboutNav').on('click', function() {
+	$('.aboutNav').on('click', function () {
 		$('.imagesDiv').hide();
 		$('.contact').hide();
 		$('.about').show();
 
 	})
-	
-	$('.contactNav').on('click', function() {
+
+	$('.contactNav').on('click', function () {
 		$('.imagesDiv').hide();
 		$('.about').hide();
 		$('.contact').show();
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var path = require("path");
+var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8081;
+
 app.use(express.static('public'))
 
-
-
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './index.html' ))
+    res.sendFile(path.join(__dirname, 'index.html' ))
 })
 
-
+// app.get('/laydowns', function(req, res) {
+//     res.sendFile(path.join(__dirname, './index.html'))
+// })
 
 app.listen(PORT, function() {
     console.log('App listening on port:' + PORT);
 })
+
