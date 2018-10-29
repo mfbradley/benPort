4ffb78e7a8ef90ca7513703bde0bf1a12e8e575d
mfbradley <mbradleystylist@gmail.com>
2018-10-29 13:56:11 -0400

    adding routes for about and contact; updated link CSS in sideNav

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,32 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html' ))
-})
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
 
-app.get('/editorial', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/editorial.html'))
-})
+    app.get('/editorial', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/editorial.html'))
+    })
 
-app.get('/laydowns', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/laydowns.html'))
-})
+    app.get('/laydowns', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/laydowns.html'))
+    })
+
+    app.get('/tableTop', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/tableTop.html'))
+    })
+
+    app.get('/gifs', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/gifs.html'))
+    })
+    app.get('/about', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/about.html'))
+    })
+
+    app.get('/contact', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"))
+    })
+
 }
